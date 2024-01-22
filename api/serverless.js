fb0a2c594fce5fc77bef0f458595b494e99b4d37
Bluce <a1019521a@gmail.com>
2024-01-22 14:02:42 +0800

    test api

--- api/serverless.js
+++ api/serverless.js
@@ -6,44 +6,44 @@ dotenv.config();
 
 // Require the framework
 import Fastify from "fastify";
-import cors from "@fastify/cors";
+//import cors from "@fastify/cors";
 
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
 
-await app.register(cors, {
-  // put your options here
-  //origin:[cors_Orgin]
-  // origin:'http://localhost:8050/'
-  origin: "http://127.0.0.1:5500",
-});
+// await app.register(cors, {
+//   // put your options here
+//   //origin:[cors_Orgin]
+//   // origin:'http://localhost:8050/'
+//   origin: "http://127.0.0.1:5500",
+// });
 
-/* rate-limit 流量限制*/
-await app.register(import("@fastify/rate-limit"), {
-  //global : false,          // default true
-  max: 3, // default 1000
-  timeWindow: "1 minute", // default 1000 * 60
-  allowList:['http://127.0.0.1:5500'],
-  errorResponseBuilder: function (request, context) {
-    return {
-      code: 429,
-      error: "Too Many Requests",
-      message: `I only allow ${context.max} requests per ${context.after} to this Website. Try again soon.`,
-      date: Date.now(),
-      expiresIn: context.ttl, // milliseconds
-    };
-  },
-});
+// /* rate-limit 流量限制*/
+// await app.register(import("@fastify/rate-limit"), {
+//   //global : false,          // default true
+//   max: 3, // default 1000
+//   timeWindow: "1 minute", // default 1000 * 60
+//   allowList:['http://127.0.0.1:5500'],
+//   errorResponseBuilder: function (request, context) {
+//     return {
+//       code: 429,
+//       error: "Too Many Requests",
+//       message: `I only allow ${context.max} requests per ${context.after} to this Website. Try again soon.`,
+//       date: Date.now(),
+//       expiresIn: context.ttl, // milliseconds
+//     };
+//   },
+// });
 
-app.setErrorHandler(function (error, request, reply) {
-  if (error.statusCode === 429) {
-    reply.code(429);
-    error.message = "You hit the rate limit! Slow down please!";
-  }
-  reply.send(error);
-});
+// app.setErrorHandler(function (error, request, reply) {
+//   if (error.statusCode === 429) {
+//     reply.code(429);
+//     error.message = "You hit the rate limit! Slow down please!";
+//   }
+//   reply.send(error);
+// });
 
 // Register your application as a normal plugin.
 app.register(import("../src/app.js"));
