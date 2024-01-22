c9438cd3c932d57a7d6dbd0d3a347b580043ef80
Bluce <a1019521a@gmail.com>
2024-01-22 11:52:53 +0800

    first vercel test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
+// import Fastify from 'fastify'
+// const fastify = Fastify({
+//   logger: true
+// })
+
+
+// export default async function routes(fastify,options){
+
+
+// }
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 // Declare a route
 fastify.get('/test', async function handler (request, reply) {
   return { hello: 'world' }
 })
 
+
 // Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
-}
+// try {
+//   await fastify.listen({ port: 3000 })
+// } catch (err) {
+//   fastify.log.error(err)
+//   process.exit(1)
+// }
