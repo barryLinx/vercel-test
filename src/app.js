b1cc0ce830382b65fe367d3b0fc0980e3a546ec8
Bluce <a1019521a@gmail.com>
2024-01-22 19:13:33 +0800

    modify vercel.json

--- src/app.js
+++ src/app.js
@@ -21,16 +21,16 @@ async function routes (fastify, options) {
 
   fastify.get('/api', async (request, reply) => {
     reply.code(200)
-    .headers({"Access-Control-Allow-Origin":"*"})
-   .headers({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"})
-   .headers({"Content-Type":"application/json; charset=utf-8"})
+  //   .headers({"Access-Control-Allow-Origin":"*"})
+  //  .headers({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"})
+  //  .headers({"Content-Type":"application/json; charset=utf-8"})
    .send({ vercel: "Vercel test" })
   });
   fastify.get('/api/test', async (request, reply) => {
     reply.code(200)
-    .header("Access-Control-Allow-Origin", "*")
-   .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-   .header("Content-Type", "application/json; charset=utf-8")
+  //   .header("Access-Control-Allow-Origin", "*")
+  //  .header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  //  .header("Content-Type", "application/json; charset=utf-8")
    .send({test:'good test'})
   });
 
