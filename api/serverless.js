018ed37fe3724ab6bc6b3fe17bb336ce12658218
Bluce <a1019521a@gmail.com>
2024-01-22 12:26:54 +0800

    Test fastify cors plugin

--- api/serverless.js
+++ api/serverless.js
@@ -6,11 +6,45 @@ dotenv.config();
 
 // Require the framework
 import Fastify from "fastify";
-//import routes from '../src/app'
+import cors from '@fastify/cors';
+
+
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
+
+await app.register(cors, { 
+  // put your options here
+  //origin:[cors_Orgin]
+   origin:'http://localhost:8050/'
+});
+
+/* rate-limit 流量限制*/
+// await app.register(import('@fastify/rate-limit'), {
+//   //global : false,          // default true
+//   max: 3,                 // default 1000
+//   timeWindow: '1 minute',// default 1000 * 60
+//   //allowList:[cors_Orgin],
+//   errorResponseBuilder: function (request, context) {
+//     return {
+//       code: 429,
+//       error: 'Too Many Requests',
+//       message: `I only allow ${context.max} requests per ${context.after} to this Website. Try again soon.`,
+//       date: Date.now(),
+//       expiresIn: context.ttl // milliseconds
+//     }
+//   }
+// });
+
+// app.setErrorHandler(function (error, request, reply) {
+//   if (error.statusCode === 429) {
+//     reply.code(429)
+//     error.message = 'You hit the rate limit! Slow down please!'
+//   }
+//   reply.send(error)
+// })
+
 
 // Register your application as a normal plugin.
 app.register(import("../src/app.js"));
